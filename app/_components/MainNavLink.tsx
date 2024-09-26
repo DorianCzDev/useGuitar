081bb2c7f27fa80fe37e1a440d749c88d1541b5d
Dorian Czupryniak <dorian.czupryniak@gmail.com>
2024-09-27 01:46:22 +0200

    change css

--- app/_components/MainNavLink.tsx
+++ app/_components/MainNavLink.tsx
@@ -17,7 +17,7 @@ function MainNavLink({ children, href }: MainNavLinkProps) {
         pathname === href
           ? "text-neutral-200 border-b border-neutral-300"
           : "text-neutral-500"
-      } visited:link:active:no-underline uppercase tracking-[4px] px-8 text-2xl py-5 hover:text-neutral-200 transition-all font-normal md:text-4xl md:text-neutral-200 md:border-none`}
+      } visited:link:active:no-underline uppercase tracking-[4px] px-8 text-2xl py-5 hover:text-neutral-200 font-normal md:text-4xl md:text-neutral-200 md:border-none transition-colors`}
     >
       {children}
     </Link>
